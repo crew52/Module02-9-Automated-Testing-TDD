903e6e49f3d2d1d2fbc672bc18a212508c06d092
crew52 <dungnc150602@gmail.com>
2024-12-17 13:44:33 +0700

    update day

--- CalculateNextDay/src/test/java/org/example/NextDayCalculatorTest.java
+++ CalculateNextDay/src/test/java/org/example/NextDayCalculatorTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class NextDayCalculatorTest {
     @Test
     @DisplayName("Test case 1/1/2018 -> 2/1/2018")
-    void testNextDay1() {
+    void testNextDay_Case1() {
         int day = 1;
         int month = 1;
         int year = 2018;
@@ -19,7 +19,7 @@ class NextDayCalculatorTest {
 
     @Test
     @DisplayName("Test case 31/1/2018 -> 1/2/2018")
-    void testNextDay2() {
+    void testNextDay_Case2() {
         int day = 31, month = 1, year = 2018;
         String expected = "1/2/2018";
         String result = NextDayCalculator.getNextDay(day, month, year);
@@ -28,7 +28,7 @@ class NextDayCalculatorTest {
 
     @Test
     @DisplayName("Test case 30/4/2018 -> 1/5/2018")
-    void testNextDay3() {
+    void testNextDay_Case3() {
         int day = 30, month = 4, year = 2018;
         String expected = "1/5/2018";
         String result = NextDayCalculator.getNextDay(day, month, year);
@@ -37,7 +37,7 @@ class NextDayCalculatorTest {
 
     @Test
     @DisplayName("Test case 28/2/2018 -> 1/3/2018")
-    void testNextDay4() {
+    void testNextDay_Case4() {
         int day = 28, month = 2, year = 2018;
         String expected = "1/3/2018";
         String result = NextDayCalculator.getNextDay(day, month, year);
@@ -46,7 +46,7 @@ class NextDayCalculatorTest {
 
     @Test
     @DisplayName("Test case 29/2/2020 -> 1/3/2020")
-    void testNextDay5() {
+    void testNextDay_Case5() {
         int day = 29, month = 2, year = 2020;
         String expected = "1/3/2020";
         String result = NextDayCalculator.getNextDay(day, month, year);
@@ -55,7 +55,7 @@ class NextDayCalculatorTest {
 
     @Test
     @DisplayName("Test case 31/12/2018 -> 1/1/2019")
-    void testNextDay6() {
+    void testNextDay_Case6() {
         int day = 31, month = 12, year = 2018;
         String expected = "1/1/2019";
         String result = NextDayCalculator.getNextDay(day, month, year);
